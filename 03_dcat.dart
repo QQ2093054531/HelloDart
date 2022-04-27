2ef9299c9c3395b7fe3c53e4fb16b759ddb4929f
QQ2093054531 <2093054531@qq.com>
2022-04-27 15:55:14 +0000

    新增

--- 03_dcat.dart
+++ 03_dcat.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:args/args.dart';
 
+const lineNumber = 'line-number';
 main(List<String> args) {
   exitCode = 0;
-  final parser = ArgParser();
+  final parser = ArgParser()..
 
   print('done.');
 }
